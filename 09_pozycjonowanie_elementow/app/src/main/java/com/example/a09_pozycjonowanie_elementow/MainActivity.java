f3d22c2ce98caba6bc97b7303e4b2b398261fecd
Pawi2323 <domagala444@o2.pl>
2022-02-01 15:57:07 +0100

    Koniec lekcji

--- 09_pozycjonowanie_elementow/app/src/main/java/com/example/a09_pozycjonowanie_elementow/MainActivity.java
+++ 09_pozycjonowanie_elementow/app/src/main/java/com/example/a09_pozycjonowanie_elementow/MainActivity.java
@@ -1,76 +1,56 @@
 package com.example.a09_pozycjonowanie_elementow;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.a09_pozycjonowanie_elementow.databinding.ActivityMainBinding;
-
+import android.telephony.TelephonyManager;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        final EditText num = (EditText) findViewById(R.id.editText);
+        Button ch = (Button) findViewById(R.id.buttonRead);
+        TelephonyManager operator = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        Button save = (Button) findViewById(R.id.buttonSave);
 
-        setSupportActionBar(binding.toolbar);
+        final String myID = ""; //When Reading The File Back, I Need To Store It In This String For Later Use
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        save.setOnClickListener(new OnClickListener() {
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                //Get Text From EditText "ID" And Save It To Internal Memory
+            }
+        });
+
+        ch.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                //Read From The Saved File Here And Append It To String "myID"
+
+
+                String hash = Uri.encode("#");
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:*141*" + /*Use The String With Data Retrieved Here*/ num.getText()
+                        + hash));
+                startActivity(intent);
             }
         });
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
-}
